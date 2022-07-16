84c3fcb0ddbc58cb6f15b5e82f9ce8209cd67041
Tochukwu <tochind29@gmail.com>
2022-07-16 03:08:52 +0100

    update

---
+++ Algorithms/challenge31.js
@@ -0,0 +1,13 @@
+// Your goal is to return multiplication table for number that is always an integer from 1 to 10.
+
+// For example, a multiplication table (string) for number == 5 looks like below:
+
+function multiTable(number) {
+    // good luck
+    let n = [];
+    for(let i = 1; i <= 10; i++){
+      n.push(`${i} * ${number} = ${i * number}`)
+    }
+    return n.join('\n')
+  }
+  console.log(multiTable(5))
